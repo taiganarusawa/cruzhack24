384ba811ba502facb86e8b2087ef5687623fea5b
wokular <only1earth4you@gmail.com>
2024-01-20 14:20:00 -0800

    college layout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import { Link } from "@mui/material";
 import styles from "@/styles/page.module.css";
 
 import Map from "@/components/map";
+import CollegeLayout from "@/components/collegeLayout";
 
 export default function Home() {
    return (
@@ -12,6 +13,7 @@ export default function Home() {
          <Link href="/c9" sx={{ backgroundColor: "red" }}>
             Click me
          </Link>
+         <CollegeLayout></CollegeLayout>
       </>
    )
 }
