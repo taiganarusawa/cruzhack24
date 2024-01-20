91f0bab2636b821439e999f9177dfca1302d4d0f
Taiga Narusawa <tigertigertaigaa@gmail.com>
2024-01-20 11:01:08 -0800

    working starting map

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,7 +1,20 @@
+import { useMemo } from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
 export default function Map() {
+   const { isLoaded } = useLoadScript({ 
+      googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, 
+   });
+
+   if (!isLoaded) return <div>Loading...</div>
+
    return (
-      <p>
-         This is da map
-      </p>
-   )
+      <GoogleMap
+         mapContainerStyle={{ width: "100vw", height: "100vh" }}
+         zoom={8}
+         center={{ lat: 37.7749, lng: -122.4194 }}
+      >
+         <Marker position={{ lat: 37.7749, lng: -122.4194 }} />
+      </GoogleMap>
+   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,9 +9,9 @@ export default function Home() {
    return (
       <>
          <Map></Map>
-         <Link href="/c9" sx={{ backgroundColor: "red" }}>
+         {/* <Link href="/c9" sx={{ backgroundColor: "red" }}>
             Click me
-         </Link>
+         </Link> */}
       </>
    )
 }
