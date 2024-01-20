42be30df5a5a4b57a4bbb09999a117b44c4ea492
Taiga Narusawa <tigertigertaigaa@gmail.com>
2024-01-20 15:00:19 -0800

    blank commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,8 +4,8 @@ import Nav from "@/components/nav";
 export default function Home() {
    return (
       <>
-         <Nav></Nav>
-         <Map></Map>
+         <Nav />
+         <Map />
       </>
    )
 }
